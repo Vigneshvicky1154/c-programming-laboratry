c35fe96b92fbe2b6ed6035a53be504a829f7d4ff
Vigneshvicky1154 <vigneshvickycsd@gmail.com>
2024-10-06 22:38:12 +0530

    Create Num and it's cube.c

---
+++ Num and it's cube.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,cube;
+    printf("num is");
+    scanf("%d",&num);
+    cube=num*num*num;
+    printf("cube of the num is %d",cube);
+    return 0;
+}
