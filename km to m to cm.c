6fc662b334788eb7993db40b2cdf21639546cdd5
Vigneshvicky1154 <vigneshvickycsd@gmail.com>
2024-10-06 19:49:31 +0530

    Create km to m to cm.c

---
+++ km to m to cm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int A;
+    scanf("%d",&A);
+    int B;
+    B=A*1000;
+    printf("%d\n",B);
+    int C;
+    C=A*100000;
+    printf("%d",C);
+
+    return 0;
+}
