62b75731c54385b693ba6d07430b2c2f113ff0f4
Vigneshvicky1154 <vigneshvickycsd@gmail.com>
2024-10-06 23:30:03 +0530

    Create Uppercase to Lowercase.c

---
+++ Uppercase to Lowercase.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char u;
+    printf("Uppercase char:%c",u);
+    scanf("%c",&u);
+    char l;
+    l=u+32;
+    printf("Lowercase char:%c",l);
+    return 0;
+}
