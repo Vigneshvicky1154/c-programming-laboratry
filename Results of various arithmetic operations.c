e254489dc9008b32841b80a2b08327aa27f4fe28
Vigneshvicky1154 <vigneshvickycsd@gmail.com>
2024-10-06 18:56:42 +0530

    Create Results of various arithmetic operations.c

---
+++ Results of various arithmetic operations.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+  printf("enter two numbers");
+  scanf("%d%d",&a,&b);
+  int sum;
+  sum=a+b;
+  printf("sum is");
+  scanf("%d",&sum);
+  int diff;
+  diff=a-b;
+  printf("diff is");
+  scanf("%d",&diff);
+  int product;
+  product=a*b;
+  printf("product is");
+  scanf("%d",&product);
+  int quo;
+  quo=a/b;
+  printf("quo is");
+  scanf("%d",&quo);
+  int rem;
+  rem=a%b;
+  printf("remaining is ");
+  scanf("%d",&rem);
+    return 0;
+}
