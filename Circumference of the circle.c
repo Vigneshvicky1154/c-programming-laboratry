e1cb44823a98a141aa1d9abc1bc8285b116da00b
Vigneshvicky1154 <vigneshvickycsd@gmail.com>
2024-10-06 19:29:38 +0530

    Create Circumference of the circle.c

---
+++ Circumference of the circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int r;
+    scanf("%d",&r);
+    float circumference;
+    circumference=2*M_PI*r;
+    printf("%.2f",circumference);
+
+    return 0;
+}
