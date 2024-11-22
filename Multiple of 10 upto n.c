b92cd50fbd255abe326b8eaa023abcd08679f6cb
Vigneshvicky1154 <vigneshvickycsd@gmail.com>
2024-11-22 21:09:05 +0530

    Create Multiple of 10 upto n.c

---
+++ Multiple of 10 upto n.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+int main()
+   {
+    int n;
+    scanf("%d",&n);
+    int v;
+    for(int i=1;i<=n;i++) 
+    {
+       v=10*i;
+       printf("%d\n",v);
+    }
+    return 0;
+}
