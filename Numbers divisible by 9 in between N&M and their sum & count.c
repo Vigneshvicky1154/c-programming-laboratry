4bbcb28310179e3287634de348aec05a697de729
Vigneshvicky1154 <vigneshvickycsd@gmail.com>
2024-11-22 21:33:42 +0530

    Update and rename Numbers divisible by 9 in between N&M.c to Numbers divisible by 9 in between N&M and their sum & count.c

--- Numbers divisible by 9 in between N&M and their sum & count.c
+++ Numbers divisible by 9 in between N&M and their sum & count.c
@@ -3,14 +3,19 @@ int main()
    {
     int n,m;
     scanf("%d%d",&n,&m);
-    int v;
+    int c=0,sum=0;
     for(int i=n;i<=m;i++) 
     {
        if(i%9==0)
        {
        printf("%d  ",i);
+       c++;
+       sum=sum+i;
        }
        
     }
+    printf("\n");
+    printf("No.of.integers %d\n",c);
+    printf("%d",sum);
     return 0;
 }
